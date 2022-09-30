32f1c0f2629677189b7a0d7da7da48ed3eea589e
DoraLC <34760377+DoraLC@users.noreply.github.com>
2022-09-30 18:45:00 +0800

    update testing code

--- test.ts
+++ test.ts
@@ -1,24 +1,14 @@
 input.onButtonPressed(Button.A, function () {
-    pv += 10
+    pv += 50
 })
 input.onButtonPressed(Button.B, function () {
-    pv += -10
+    pv += -50
 })
-let pidvalue2 = 0
 let pidvalue = 0
-let pv = 0.1
-let pv2 = 0.1
+let pv = 50 + randint(-5, 5)
 let pid2 = pid.create(
     50,
-    1.5,
-    0.0001,
-    0.04,
-    100,
-    -100
-)
-let pid22 = pid.create(
-    50,
-    1.5,
+    0.999,
     0.0001,
     0.04,
     100,
@@ -28,11 +18,6 @@ basic.forever(function () {
     pidvalue = pid2.pid_value(pv)
     pv += -1 * pidvalue
     serial.writeValue("pv1", pv)
-    pv += randint(-1, 1)
-})
-basic.forever(function () {
-    pidvalue2 = pid22.pid_value(pv2)
-    pv2 += -1 * pidvalue2
-    serial.writeValue("pv2", pv2)
-    pv2 += randint(-1, 1)
+    pv += 10
+    pv += randint(-10, 10)
 })
